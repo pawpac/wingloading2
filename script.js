39b085d3284477e55fd86c6f3d3d361cb80c4b45
Pawel Paczkowski <paw.pac@yahoo.com>
2022-11-01 11:02:02 -0500

    Rig sliders with display

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const exitSlider = document.querySelector('.exit-slider');
 const canopyDigits = document.querySelector('.canopy-digit');
 const exitDigits = document.querySelector('.exit-digit');
 const canopy = document.querySelector('.canopy');
+const wingloading = document.querySelector('.wingloading-digit');
+const level = document.querySelector('.level-digit');
 
 // LINES
 const leftLines = document.querySelector('.lines-point-left');
@@ -27,6 +29,18 @@ sizeSlider.oninput = function() {
   canopyDigits.textContent = sizeSlider.value;
   let newCanopySlider = parseInt(sizeSlider.value);
 
+  wingloading.textContent = (exitSlider.value / sizeSlider.value).toFixed(2);
+
+  if ((wingloading.textContent < 1.8) && (wingloading.textContent >= 1.4)) {
+    level.textContent = 'Advanced';
+  } else if ((wingloading.textContent < 1.4) && (wingloading.textContent >= 1)) {
+    level.textContent = 'Intermediate';
+  } else if ((wingloading.textContent < 1)) {
+    level.textContent = 'Novice';
+  } else {
+    level.textContent = 'Master';
+  }
+
   canopySize = canopySize + ((newCanopySlider - initialCanopySlider) * 0.217391304);
   leftLinesRotation = leftLinesRotation + ((newCanopySlider - initialCanopySlider) * (- 0.095652174));
   rightLinesRotation = rightLinesRotation + ((newCanopySlider - initialCanopySlider) * 0.095652174);
@@ -41,6 +55,18 @@ sizeSlider.oninput = function() {
 exitSlider.oninput = function () {
   exitDigits.textContent = exitSlider.value;
   let newExitSlider = parseInt(exitSlider.value);
+
+  wingloading.textContent = (exitSlider.value / sizeSlider.value).toFixed(2);
+
+  if ((wingloading.textContent < 1.8) && (wingloading.textContent >= 1.4)) {
+    level.textContent = 'Advanced';
+  } else if ((wingloading.textContent < 1.4) && (wingloading.textContent >= 1)) {
+    level.textContent = 'Intermediate';
+  } else if ((wingloading.textContent < 1)) {
+    level.textContent = 'Novice';
+  } else {
+    level.textContent = 'Master';
+  }
 
   exitWeight = exitWeight + ((newExitSlider - initialExitSlider) * 0.15);
 
